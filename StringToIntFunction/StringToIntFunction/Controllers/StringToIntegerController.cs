8701019dce99ee0741cd8fb67b72db0cf6c300f4
wiriya patanaratcharoen <wiriya.patanarat@gmail.com>
2021-07-23 01:03:59 +0700

    Create project netcoreapp 3.1, add gitignore, create template API

---
+++ StringToIntFunction/StringToIntFunction/Controllers/StringToIntegerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StringToIntFunction.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StringToIntegerController : ControllerBase
+    {
+        public StringToIntegerController()
+        {
+
+        }
+
+        [HttpGet("StringToInteger")]
+        public int StringToInteger(string text)
+        {
+            int result = 0;
+            try
+            {
+                // TODO: coding here
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+        }
+    }
+}
