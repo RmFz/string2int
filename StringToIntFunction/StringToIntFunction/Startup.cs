46e0a4623d0dd20515d9a657750110c39fb18420
wiriya patanaratcharoen <wiriya.patanarat@gmail.com>
2021-07-23 01:12:11 +0700

    Add swagger

--- StringToIntFunction/StringToIntFunction/Startup.cs
+++ StringToIntFunction/StringToIntFunction/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace StringToIntFunction
 {
@@ -26,6 +27,14 @@ namespace StringToIntFunction
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "StringToInteger API"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +44,12 @@ namespace StringToIntFunction
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(config =>
+            {
+                config.SwaggerEndpoint("/swagger/v1/swagger.json", "StringToInteger API");
+            });
 
             app.UseHttpsRedirection();
 
